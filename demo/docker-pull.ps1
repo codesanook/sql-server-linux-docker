64879a2446020283faa29584a5decf08ab3e59cc
aaronamm <theeranitp@gmail.com>
2020-05-22 15:39:21 +0700

    Add additional docker commands

--- demo/docker-pull.ps1
+++ demo/docker-pull.ps1
@@ -1,4 +1,6 @@
 docker pull mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
 
-# docker image rm mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
-# docker image ls
+
+# addtional commands
+docker image rm mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+docker image ls
--- demo/docker-run.ps1
+++ demo/docker-run.ps1
@@ -1,1 +1,5 @@
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=12345Abc%" -p 1401:1433 --name sqlserver mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+
+
+# addtional commands
+docker ps --format "table {{.Id}}\t{{.Names}}\{{.Status}}"
